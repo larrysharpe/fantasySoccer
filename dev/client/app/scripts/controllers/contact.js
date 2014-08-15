9fc4b4a2b7716dabae2117ba00cf248c46acd56a
larry.sharpe <larry.l.sharpe@gmail.com>
2014-08-15 12:30:36 -0400

    Initial Commit

---
+++ dev/client/app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contactApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the contactApp
+ */
+angular.module('contactApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
